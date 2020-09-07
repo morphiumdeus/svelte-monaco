90d6c35e9d7eedd7cc4208cd0f11a7173761a67d
morphiumdeus <morphiumdeus@gmail.com>
2020-09-07 19:57:26 +0200

    yjs test

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script>
 	import Editor from './Editor.svelte'
+	import yjs from './yjs.svelte'
 </script>
 
 <Editor />
--- src/Editor.svelte
+++ src/Editor.svelte
@@ -4,10 +4,7 @@
     onDestroy } from 'svelte'
   import { userCSS, showEditor } from './store'
   // import { editor as MonacoEditor } from 'monaco-editor';
-  import { pannable } from './pannable.js';
   let editor, modelChangeSub
-  let x = 0
-  let y = 0
   // onMount(mountEditor)
   function mountEditor() {
     setTimeout(() => {
@@ -48,67 +45,13 @@
     if (modelChangeSub) modelChangeSub.dispose()
   })
 
-
-
-  function handlePanMove(event) {
-    x = x + event.detail.dx
-    y = y + event.detail.dy
-  }
-  function toggleEditor() {
-    $showEditor = !$showEditor
-    if ($showEditor) mountEditor()
-  }
 </script>
 
 <style>
-  #editorWithButton {
-    height: 100vh;
-    width: 40vw;
-    min-width: 600px;
-    position: fixed;
-    z-index: 999;
-    bottom: 0;
-    left: 20vw;
-  }
   #monaco-container {
     height: 100%;
     width: 100%;
   }
-  .handButton {
-    position: fixed;
-    width: 60px;
-    height: 60px;
-    z-index: 9999;
-    top: -30px;
-    right: -30px;
-    border-radius: 50px;
-    text-align: center;
-    box-shadow: 2px 2px 3px #999;
-    font-size: 3rem;
-  }
-  .open {
-    background-color: #0C9;
-  }
-  .close {
-    background-color: #F55;
-  }
 </style>
-<div use:pannable
-  on:panmove={handlePanMove}
-  style="transform:
-  translate({x}px,{y}px)"
-  id="editorWithButton"
->
-  {#if $showEditor}
-  <button class="handButton close" on:click={toggleEditor}>
-    ✖️    
-  </button>
-  {:else}
-  <button class="handButton open" on:click={toggleEditor}>
-    ✍️
-  </button>
-  {/if}
-  {#if $showEditor}
-  <div id="monaco-container"></div>
-  {/if}
-</div>
+
+<div id="monaco-container"></div>
