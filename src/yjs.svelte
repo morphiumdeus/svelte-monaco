068275afb2811ec7e31df539b47dbfd7c7043c9e
morphiumdeus <morphiumdeus@gmail.com>
2020-09-10 10:02:10 +0200

    test

--- src/yjs.svelte
+++ src/yjs.svelte
@@ -4,7 +4,7 @@ import { WebrtcProvider } from 'y-webrtc'
 import { WebsocketProvider } from 'y-websocket'
 import { IndexeddbPersistence } from 'y-indexeddb'
 
-console.log("tada")
+console.log("tada2")
 const ydoc = new Y.Doc()
 
 // this allows you to instantly get the (cached) documents data
