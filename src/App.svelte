61ff08941bb1b42409f48c6e6feea802d6b050d6
morphiumdeus <morphiumdeus@gmail.com>
2020-09-13 20:44:10 +0200

    fix roomname spaces

--- src/App.svelte
+++ src/App.svelte
@@ -4,7 +4,8 @@
 	import { roomName } from './store'
 	const greg = require("greg")
 
-	$roomName = greg.sentence()
+	const sentence = greg.sentence()
+	$roomName = sentence.replace(" ", "-")
 	console.log("The sentence \"%s\" corresponds to the id %d", $roomName, greg.parse($roomName))
 </script>
 
