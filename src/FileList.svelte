06d322ce4eecba1b24f02bbc2e03cc3609a61e25
morphiumdeus <morphiumdeus@gmail.com>
2020-09-15 15:20:25 +0200

    still ydoc

--- src/FileList.svelte
+++ src/FileList.svelte
@@ -2,9 +2,9 @@
     import { roomName, ydoc, activeFile } from './store'
     let files = {}
     const unsubscribe = ydoc.subscribe(value => {
-        if(undefined !== $ydoc.getMap('fileMap')){
-            if(undefined !== $ydoc.getMap('fileMap').getMap('files')){
-                files = $ydoc.getMap('fileMap').getMap('files')
+        if(undefined !== value.getMap('fileMap')){
+            if(undefined !== value.getMap('fileMap').getMap('files')){
+                files = value.getMap('fileMap').getMap('files')
                 console.log(files);
             }
         }
