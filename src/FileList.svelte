e1769b9d6918f25d4cb6a7542d301cbf8b9ae217
morphiumdeus <morphiumdeus@gmail.com>
2020-09-15 15:15:50 +0200

    still ydoc

--- src/FileList.svelte
+++ src/FileList.svelte
@@ -1,9 +1,9 @@
 <script>
     import { roomName, ydoc, activeFile } from './store'
     let files = {}
-    $: if(undefined !== ydoc.getMap('fileMap')){
-        if(undefined !== ydoc.getMap('fileMap').getMap('files')){
-            files = ydoc.getMap('fileMap').getMap('files')
+    $: if(undefined !== $ydoc.getMap('fileMap')){
+        if(undefined !== $ydoc.getMap('fileMap').getMap('files')){
+            files = $ydoc.getMap('fileMap').getMap('files')
         }
     }
 </script>
