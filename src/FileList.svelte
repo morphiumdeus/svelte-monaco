56ef87177b712b61d1ef6c540fd60aa36b4764f0
morphiumdeus <morphiumdeus@gmail.com>
2020-09-15 22:08:36 +0200

    ydoc

--- src/FileList.svelte
+++ src/FileList.svelte
@@ -3,7 +3,7 @@
 
     let files = { has: () => false}
     const unsubscribe = ydoc.subscribe(value => {
-        console.log(value)
+        console.log(value, $ydoc)
         if(value.getMap('fileMap') && value.getMap('fileMap').has('files'))
         files = value.getMap('fileMap')
     })
